dd7bed380c98afadae63ebd67032888495a65d1b
yooha1003 <uksu@Uksus-MacBook-Pro.local>
2020-07-27 18:44:59 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gisd',
+    version='1.0.0',
+    url='https://github.com/yooha1003/gisd',
+    author='Uksu, Choi',
+    author_email='qtwing@naver.com',
+    description='Google image searching and downloading script',
+    packages=find_packages(),
+    install_requires=['selenium', 'argparse', 'tqdm', 'time', 'requests', 'urllib', 'array'],
+)
